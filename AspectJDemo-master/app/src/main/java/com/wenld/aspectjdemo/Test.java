cf2d898451b62ff2b2da8b0769857d040dbbf422
yzp531 <yzp531@163.com>
2018-01-05 17:43:57 +0800

    l

--- AspectJDemo-master/app/src/main/java/com/wenld/aspectjdemo/Test.java
+++ AspectJDemo-master/app/src/main/java/com/wenld/aspectjdemo/Test.java
@@ -10,11 +10,7 @@ public abstract class Test {
     public void method(){
         Log.e("childMethod","before...");
         childMethod();
-<<<<<<< HEAD
-//1
-=======
-        //00000
->>>>>>> 4d8007ab069e28638b8b26b0f5a7e1d7c43d11c3
+
         Log.e("childMethod","after...");
     }
     abstract void childMethod();
