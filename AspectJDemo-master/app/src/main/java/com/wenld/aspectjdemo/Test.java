f076a9edd079ddbaa1f62e414b06896af6549c70
yzp531 <yzp531@163.com>
2018-01-08 14:18:30 +0800

    Revert "444"
    This reverts commit 77cb317f3e17be44a8152a04336c8dc991c3ea59.

--- AspectJDemo-master/app/src/main/java/com/wenld/aspectjdemo/Test.java
+++ AspectJDemo-master/app/src/main/java/com/wenld/aspectjdemo/Test.java
@@ -10,6 +10,12 @@ public abstract class Test {
     public void method(){
         Log.e("childMethod","before...");
         childMethod();
+
+
+        //2
+ 
+        //0
+
         Log.e("childMethod","after...");
     }
     abstract void childMethod();
